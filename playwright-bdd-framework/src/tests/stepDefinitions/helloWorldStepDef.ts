d5b90f5799ee89462d15ff211bac1527dc36ead4
SaiG9966 <saiteja94409@gmail.com>
2026-03-12 12:06:06 +0530

    Initial commit

--- playwright-bdd-framework/src/tests/stepDefinitions/helloWorldStepDef.ts
+++ playwright-bdd-framework/src/tests/stepDefinitions/helloWorldStepDef.ts
@@ -1,13 +1,2 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
-
-Given('I have a configured Cucumber setup', function () {
-    // Setup logic here
-});
-
-When('I run the tests', function () {
-    // Test execution logic here
-});
-
-Then('I should see the results', function () {
-    // Result verification logic here
-});
+// Intentionally empty for fresh framework template.
+export {};
